997988db65e69b42e56a74a037b7fc519ad7a6b0
lucas.moreira <lucasmoreira.gabriel2004@gmail.com>
2024-09-06 16:37:19 -0300

    add rest template

--- hr-payroll/src/main/java/com/github/lucasgms/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/github/lucasgms/hr_payroll/services/PaymentService.java
@@ -1,15 +1,34 @@
 package com.github.lucasgms.hr_payroll.services;
 
 import com.github.lucasgms.hr_payroll.entities.Payment;
+import com.github.lucasgms.hr_payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
-    public Payment getPayment(long workerId, int days) {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(Long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("id", workerId.toString());
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
 
         return new Payment(
-            "Lucas", 100.0, days
+            worker.getName(), worker.getDailyIncome(), days
         );
     }
 }
